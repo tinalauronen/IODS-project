3a669a6d50085826afecc2f5ce82f406eba30b12
tinalauronen <tlaurone@Tina-MacBook-Pro.local>
2017-02-23 17:13:18 +0200

    RStudio 5
    Data wrangling

--- data/create_human.R
+++ data/create_human.R
@@ -2,6 +2,8 @@
 # Tina Lauronen
 # 16.2.2017
 
+# RStudio Exercise 5: Data wrangling
+
 ## Reading the data
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
@@ -59,4 +61,52 @@ names(human)
 str(human)
 dim(human)
 
-write_csv(human, "/Users/tlaurone/GitHub/tinalauronen/IODS-project/data/human.csv")
+write.csv(human, "/Users/tlaurone/GitHub/tinalauronen/IODS-project/data/human.csv")
+
+### RSTUDIO EXERCISE 5 
+
+library(stringr)
+library(dplyr)
+
+# Checking the data: 195 observations, 19 variables
+
+dim(human)
+
+# Mutating the variable GNI to numeric, removing commas
+
+str(human$GNI)
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric() -> human$GNI
+str(human$GNI)
+
+# Excluding unneeded variables
+
+names(human)
+
+keep <- c("Country", "Educ_sec_FM_ratio", "Labour_FM_ratio", "Educ_e", "Life_e", "GNI", "MM_ratio", "Ad_birth_rate", "Rep_parl")
+human <- dplyr::select(human, one_of(keep))
+
+dim(human)
+
+# Removing observations with missing values
+
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human_ <- filter(human, complete.cases(human))
+
+dim(human_)
+
+# Removing the regions
+
+tail(human_, 10)
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+dim(human_)
+
+# Setting country as rowname, removing the variable
+
+rownames(human_) <- human_$Country
+human_ <- dplyr::select(human_, -Country)
+
+dim(human_)
+
+write.csv(human, "/Users/tlaurone/GitHub/tinalauronen/IODS-project/data/human.csv", row.names = TRUE)
